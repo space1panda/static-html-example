9130b1de082fd7c8b9379e788a1c31f79e0974d4
Tom Wilson <tom@jackhq.com>
2013-11-02 07:27:02 -0400

    added jshint connect uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,38 @@ module.exports = function(grunt) {
         src: ['app/foo.js', 'app/bar.js'],
         dest: 'js/app.js'
       }
+    },
+    jshint: {
+      dev: ['app/**/*.js']
+    },
+    uglify: {
+      dev: {
+        src: 'js/app.js',
+        dest: 'js/app.min.js'
+      }
+    },
+    watch: {
+      dev: {
+        files: ['app/**/*.js'],
+        tasks: ['jshint', 'concat']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          base: '.',
+          port: 3000
+        }
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  
+  grunt.registerTask('server', ['connect', 'watch'])
+  grunt.registerTask('default', ['jshint','concat']);
 }
